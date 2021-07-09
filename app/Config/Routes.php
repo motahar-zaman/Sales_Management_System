8d0997b78ae41b00b83c144ff5a719ee40c1ec6d
XI <khchow@xandi.co.jp>
2021-07-09 10:57:20 +0600

    create basic routes for default, home, login UI, login action and logout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,6 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
 
 /*
  * --------------------------------------------------------------------
@@ -51,3 +50,9 @@ if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
 {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
+
+$routes->get('/', 'Authentication\AuthController::index');
+$routes->get('login', 'Authentication\AuthController::login');
+$routes->post('login', 'Authentication\AuthController::loginAction');
+$routes->get('logout', 'Authentication\AuthController::logout');
+$routes->get('home', 'Authentication\AuthController::index');
