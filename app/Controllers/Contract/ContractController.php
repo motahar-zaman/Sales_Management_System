4565c29ca730afe03d8ab37cc354a9bc523f5ce6
XI <khchow@xandi.co.jp>
2021-07-13 16:41:47 +0600

    Controller works for contract details route

--- app/Controllers/Contract/ContractController.php
+++ app/Controllers/Contract/ContractController.php
@@ -5,6 +5,7 @@ namespace App\Controllers\Contract;
 
 use App\Controllers\BaseController;
 use App\Models\Contract\ContractModel;
+use App\Models\Contractor\ContractorModel;
 
 class ContractController extends BaseController
 {
@@ -14,6 +15,30 @@ class ContractController extends BaseController
             $contract = (new ContractModel())->getContractApprovalList();
             return view("Contract/contractApprovalList", ["title" => "contract Approval List", "contracts" => $contract]);
         } else {
+            return redirect()->to("/login");
+        }
+    }
+
+    public function contractDetails($contractId){
+        if( session() && session()->get('login') ){
+            $contract = (new ContractModel())->getContractById($contractId);
+            $contractDetails = $contract[$contractId] ?? null;
+
+            $contractorDetails = null;
+
+            if(isset($contractDetails) && $contractDetails->getContractorId() ){
+                $contractorDetails = (new ContractorModel())->getContractorDetailsById($contractDetails->getContractorId())[0] ?? null;
+            }
+
+            $data = array(
+                "title" => "Contract Details",
+                "contract" => $contractDetails,
+                "contractorDetails" => $contractorDetails,
+            );
+
+            return view("Contract/contractDetails", $data);
+        }
+        else{
             return redirect()->to("/login");
         }
     }
